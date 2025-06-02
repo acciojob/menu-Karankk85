1d56589645ac87cd09dd550ae45e3eb4bfde0af0
Karankk85 <karkk8533@gmail.com>
2025-06-02 13:48:23 +0530

    Update data.js
    update

--- src/components/data.js
+++ src/components/data.js
@@ -1,1 +1,47 @@
 
+const menuItems = [
+  {
+    id: 1,
+    name: 'Pancakes',
+    category: 'Breakfast',
+    image: 'https://via.placeholder.com/150',
+    price: '$5.99'
+  },
+  {
+    id: 2,
+    name: 'Burger',
+    category: 'Lunch',
+    image: 'https://via.placeholder.com/150',
+    price: '$8.99'
+  },
+  {
+    id: 3,
+    name: 'Milkshake',
+    category: 'Shakes',
+    image: 'https://via.placeholder.com/150',
+    price: '$4.99'
+  },
+  {
+    id: 4,
+    name: 'Omelette',
+    category: 'Breakfast',
+    image: 'https://via.placeholder.com/150',
+    price: '$3.99'
+  },
+  {
+    id: 5,
+    name: 'Steak Sandwich',
+    category: 'Lunch',
+    image: 'https://via.placeholder.com/150',
+    price: '$9.99'
+  },
+  {
+    id: 6,
+    name: 'Strawberry Shake',
+    category: 'Shakes',
+    image: 'https://via.placeholder.com/150',
+    price: '$4.49'
+  }
+];
+
+export default menuItems;
