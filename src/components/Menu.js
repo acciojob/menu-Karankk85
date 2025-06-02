f00c81856d8958630227f5c649c6e99e3e8eb7e5
Karankk85 <karkk8533@gmail.com>
2025-06-02 13:48:46 +0530

    Update Menu.js
    updaate

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,24 @@
 
+import React from 'react';
+
+const Menu = ({ items }) => {
+  return (
+    <div className="menu">
+      {items.map(item => (
+        <div
+          key={item.id}
+          className="menu-item"
+          data-test-id={`menu-item-${item.category.toLowerCase()}`}
+        >
+          <img src={item.image} alt={item.name} />
+          <div className="menu-info">
+            <h4>{item.name}</h4>
+            <p>{item.price}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
